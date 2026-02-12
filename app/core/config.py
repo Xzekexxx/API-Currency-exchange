3077aee43c0eb579281cc21d05787a2c938e848b
Xzekexxx <arturkazanrt@gmail.com>
2026-02-12 18:36:41 +0300

    add docker

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,15 @@
 from dotenv import find_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=find_dotenv())
+    model_config = SettingsConfigDict()
 
-    DB_HOST: str
-    DB_PORT: str
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
+    DB_HOST: str = "localhost"
+    DB_PORT: str = "5432"
+    DB_USER: str = "postgres"
+    DB_PASS: str = "mypassword"
+    DB_NAME: str = "my_db"
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
@@ -19,4 +20,7 @@ class Settings(BaseSettings):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
 
-settings = Settings()
+if os.path.exists('/.dockerenv'):
+    settings = Settings()
+else:
+    settings = Settings(_env_file=find_dotenv())
